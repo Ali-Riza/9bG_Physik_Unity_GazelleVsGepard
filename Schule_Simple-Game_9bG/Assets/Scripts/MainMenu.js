dc2f7a6bf3179311354503fe997cf5342c8c2e70
Ali-Riza <aliriza.bagci@outlook.de>
2018-11-30 00:34:56 +0100

    projekt fertig

--- Schule_Simple-Game_9bG/Assets/Scripts/MainMenu.js
+++ Schule_Simple-Game_9bG/Assets/Scripts/MainMenu.js
@@ -1,6 +1,17 @@
-﻿function PlayGame ()
+﻿function PlayGame50 ()
 {
 	Application.LoadLevel(1);
+}
+
+
+function PlayGame100 ()
+{
+	Application.LoadLevel(2);
+}
+
+function PlayGame200 ()
+{
+	Application.LoadLevel(3);
 }
 
 function ExitGame ()
